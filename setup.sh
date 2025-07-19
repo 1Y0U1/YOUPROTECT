7fb3c0fa236e7e177643531c6fc7e4c08bead2d0
Y0U <rubalsharma507@gmail.com>
2025-07-19 13:44:43 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+clear
+
+echo -e "\e[5m\e[1;34m$(figlet 'YOUPROTECT')\e[0m"
+echo Setup Starting........
+
+sudo apt-get install ruby   
+echo ""
+sudo apt install readpe
+echo ""
+sudo gem install colorize
+echo ""
+sudo gem install readelf
+echo ""
+sudo cp youprotect /usr/bin
+sudo chmod +x /usr/bin/youprotect
+echo "\n***done***\n"
+clear
+echo ""
+echo Setup Complete GO AHEAD!!!!!!        
+echo ""
+echo "----->Now you can use youprotect from everywhere<-----"
